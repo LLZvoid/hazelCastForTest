f1111a197ebc426868a0ebd4fbf4dc65e26e2f1a
LLZvoid <1138039975@qq.com>
2023-07-18 15:02:51 +0800

    hazel完善

--- src/main/java/com/llzdemo/hazelcastfortest/util/common/pre/PostConstructUtil.java
+++ src/main/java/com/llzdemo/hazelcastfortest/util/common/pre/PostConstructUtil.java
@@ -3,9 +3,11 @@ package com.llzdemo.hazelcastfortest.util.common.pre;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.topic.ITopic;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 
 /**
  * @author llz
@@ -15,10 +17,25 @@ public class PostConstructUtil {
     String selfCode;
     Integer runningCode;
 
+    @Resource
+    private HazelcastInstance hazelcastInstance;
+
     @PostConstruct
     public void tt() {
+        this.ttt();
 
+        ITopic<Object> firstMsg = hazelcastInstance.getTopic("myFirstMsg");
 
+        firstMsg.publish("give me five");
+
+    }
+
+    public void ttt() {
+        ITopic<Object> firstMsg = hazelcastInstance.getTopic("myFirstMsg");
+
+        firstMsg.addMessageListener(message -> {
+            System.out.println("hi guys");
+        });
 
     }
 }
